4819c6d35d3ae29098055e894b08a100756292c2
Abdullah Momoh <momohabdullah20@gmail.com>
2024-09-18 05:08:59 +0100

    updated the develop branch

--- blocks/navbar/navbar20.tsx
+++ blocks/navbar/navbar20.tsx
@@ -12,6 +12,7 @@ import {
 } from "lucide-react";
 import { useState } from "react";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
 
 export const Navbar20 = () => {
   const navigationItems = [
@@ -54,7 +55,6 @@ export const Navbar20 = () => {
 
         {/* Button Section */}
         <div className="hidden lg:flex justify-center lg:justify-end w-full gap-2">
-          <Button className="rounded-none">Button</Button>
           <Button
             variant="outline"
             className="rounded-none"
@@ -67,10 +67,89 @@ export const Navbar20 = () => {
       {isOpen && (
         <div className="absolute flex flex-col w-full h-svh right-0 bg-background px-10 gap-8">
           <div className="flex flex-col h-full">
-            <div className="flex flex-col gap-1 w-full h-3/4">
-              <div className="flex flex-row gap-6 w-full justify-center pb-8">
-                <div className="flex flex-col gap-1 capitalize text-2xl">
-                  <Link href={"/"}>Link One</Link>
+            <div className="flex flex-row justify-between gap-1 w-full h-3/4">
+              <div className="flex flex-col place-content-center grow-0">
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link One</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Three</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Five</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Seven</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Nine</Link>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col place-content-center grow-0">
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Two</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Four</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Six</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Eight</Link>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-6 w-full justify-start pb-8">
+                  <div className="flex flex-col gap-1 capitalize text-2xl">
+                    <Link href={"/"}>Link Ten</Link>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col place-content-center grow-1">
+                <p>Subscribe</p>
+                <p>
+                  Join our newsletter to stay up to date on features and
+                  releases
+                </p>
+                <div className="flex justify-start gap-4">
+                  <Input className="rounded-none hover:outline-0 hover:border-0" />
+                  <Button className="rounded-none">Subscribe</Button>
+                </div>
+                <p>
+                  By subscribing you agree to our Privacy Policy and provide
+                  consent to receive updates from out company
+                </p>
+                <div className="flex flex-col pb-4">
+                  <div className="flex flex-col gap-2">
+                    <p>Get in touch</p>
+                    <p>1800 123 4567</p>
+                    <p>info@firestrap.io</p>
+                    <p>Level 1, 12 Sample St, Sydney NSW 2000</p>
+                  </div>
+                  <div className="flex gap-2">
+                    <FacebookIcon className="mt-2 text-primary" />
+                    <InstagramIcon className="mt-2 text-primary" />
+                    <TwitterIcon className="mt-2 text-primary" />
+                    <LinkedinIcon className="mt-2 text-primary" />
+                    <YoutubeIcon className="mt-2 text-primary" />
+                  </div>
                 </div>
               </div>
             </div>
